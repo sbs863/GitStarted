bb9cfbad07a1ae307fe0ece1cf552541f4302d01
Sam Smith <samtrey@gmail.com>
2016-09-21 19:47:44 -0500

    front end almost done

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -35,9 +35,14 @@
         for (var i = 0; i < myArray.length; i++) {
             var r = $('<input/>').attr({
                 type: "button",
-                id: array[i]
+                class: "btn-lg btn-danger shadow",
+                autocomplete: "off",
+                id: myArray[i],
+                value: myArray[i]
             });
-            $('.right').append(r);
+            r.attr('data-toggle', "button");
+            r.attr('aria-pressed', 'false');
+            $('.buttons').append(r);
         }
     });
 
@@ -72,94 +77,26 @@
     /*geolocation*/
     $("#locate").on("click", function() {
 
+        var options = {
+            enableHighAccuracy: true,
+            timeout: 5000,
+            maximumAge: 0
+        };
 
-        function prompt(window, pref, message, callback) {
-            let branch = Components.classes["@mozilla.org/preferences-service;1"]
-                .getService(Components.interfaces.nsIPrefBranch);
+        function success(pos) {
+            var crd = pos.coords;
 
-            if (branch.getPrefType(pref) === branch.PREF_STRING) {
-                switch (branch.getCharPref(pref)) {
-                    case "always":
-                        return callback(true);
-                    case "never":
-                        return callback(false);
-                }
-            }
-
-            let done = false;
-
-            function remember(value, result) {
-                return function() {
-                    done = true;
-                    branch.setCharPref(pref, value);
-                    callback(result);
-                }
-            }
-
-            let self = window.PopupNotifications.show(
-                window.gBrowser.selectedBrowser,
-                "geolocation",
-                message,
-                "geo-notification-icon", {
-                    label: "Share Location",
-                    accessKey: "S",
-                    callback: function(notification) {
-                        done = true;
-                        callback(true);
-                    }
-                }, [{
-                    label: "Always Share",
-                    accessKey: "A",
-                    callback: remember("always", true)
-                }, {
-                    label: "Never Share",
-                    accessKey: "N",
-                    callback: remember("never", false)
-                }], {
-                    eventCallback: function(event) {
-                        if (event === "dismissed") {
-                            if (!done) callback(false);
-                            done = true;
-                            window.PopupNotifications.remove(self);
-                        }
-                    },
-                    persistWhileVisible: true
-                });
+            console.log('Your current position is:');
+            console.log('Latitude : ' + crd.latitude);
+            console.log('Longitude: ' + crd.longitude);
+            console.log('More or less ' + crd.accuracy + ' meters.');
         }
 
-        prompt(window,
-            "extensions.foo-addon.allowGeolocation",
-            "Foo Add-on wants to know your location.",
-            function callback(allowed) { alert(allowed); });
-
-        function geoFindMe() {
-            var output = document.getElementById("out");
-
-            if (!navigator.geolocation) {
-                output.innerHTML = "<p>Geolocation is not supported by your browser</p>";
-                return;
-            }
-
-            function success(position) {
-                var latitude = position.coords.latitude;
-                var longitude = position.coords.longitude;
-
-                output.innerHTML = '<p>Latitude is ' + latitude + '° <br>Longitude is ' + longitude + '°</p>';
-
-                var img = new Image();
-                img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + latitude + "," + longitude + "&zoom=13&size=300x300&sensor=false";
-
-                output.appendChild(img);
-            }
-
-            function error() {
-                output.innerHTML = "Unable to retrieve your location";
-            }
-
-            output.innerHTML = "<p>Locating…</p>";
-
-            navigator.geolocation.getCurrentPosition(success, error);
+        function error(err) {
+            console.warn('ERROR(' + err.code + '): ' + err.message);
         }
+
+        navigator.geolocation.getCurrentPosition(success, error, options);
 
         $.ajax({ url: queryURL, method: 'GET' })
             .done(function(response) {
